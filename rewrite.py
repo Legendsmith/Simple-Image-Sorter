b39e10c9967c2960b065abd789db5d698f72dea3
Legendsmith <legendsmith.au@gmail.com>
2022-01-25 17:41:09 +1100

    Now we have classes but it doesn't display images

--- rewrite.py
+++ rewrite.py
@@ -1,15 +1,12 @@
-from cgitb import text
 import os
 import shutil
 from sys import exit
-from tkinter import HORIZONTAL, messagebox
+from tkinter import HORIZONTAL, RAISED, PhotoImage, messagebox
 from shutil import move as shmove
 import tkinter as tk
 import tkinter.scrolledtext as tkst
-from tkinter.ttk import Panedwindow
-from turtle import bgcolor
-from unicodedata import name
-from PIL import Image
+from tkinter.ttk import Panedwindow, Checkbutton
+from PIL import Image, ImageTk
 from functools import partial
 from math import floor
 import json
@@ -20,7 +17,9 @@ import tkinter.font as tkfont
 from collections import deque
 from canvasimage import CanvasImage
 import concurrent.futures as concurrent
-
+import uuid
+import logging
+from autoscrollbar import AutoScrollbar
 
 class Imagefile:
     name=""
@@ -30,22 +29,15 @@ class Imagefile:
     def __init__(self,name,path) -> None:
         self.name=name
         self.path=path
-        self.thumbnail=None
+        self.id=str(uuid.uuid1())
         self.checked=tk.StringVar()
-        # Make a thumbnail
     def move(self):
         if self.dest != "":
             try:
                 shmove(self.path, os.path.join(self.dest,self.name))
                 self.moved=True
-            except FileExistsError:
-                print("An Error occured when moving ", self.name, " to destination ", self.dest, "File Already Exists")
-            except FileNotFoundError:
-                print("An Error occured when moving ", self.name, "The file has been moved or deleted already")
-
-def generatethumbnails(images):
-    with concurrent.ThreadPoolExecutor(max_workers=3) as executor:
-        executor.map()
+            except Exception as e:
+                logging.error("Error: %s . File: %s",e,self.name)
 
     
 
@@ -88,97 +80,21 @@ def saveprefs(manager,gui):
     try:
         with open("prefs.json", "w+") as savef:
             json.dump(save,savef)
-            print(save)
+            logging.debug(save)
     except Exception:
         pass
 
-class SortImages():
-    imagelist = deque()
-    destinations=[]
-    exclude=[]
-    def __init__(self) -> None:
-        self.gui = GUIManager(self)
-        if(os.path.exists("data") and os.path.isdir("data")):
-            pass
-        else:
-            os.mkdir("data")
-        hotkeys = ""
-        try:
-            with open("prefs.json","r") as prefsfile:
-                jdata=prefsfile.read()
-                jprefs=json.loads(jdata)
-                if 'hotkeys' in jprefs:
-                    hotkeys = jprefs["hotkeys"]
-                if 'thumbnailgrid' in jprefs:
-                    self.gui.thumbnailgrid = jprefs["thumbnailgrid"]
-                if 'thumbnailsize' in jprefs:
-                    self.gui.thumbnailsize = jprefs["thumbnailsize"]
-                self.exclude = jprefs["exclude"]
-                self.gui.sdpEntry.delete(0,len(self.gui.sdpEntry.get()))
-                self.gui.ddpEntry.delete(0,len(self.gui.ddpEntry.get()))
-                self.gui.sdpEntry.insert(0,jprefs["srcpath"])
-                self.gui.ddpEntry.insert(0,jprefs["despath"])
-            if len(hotkeys) > 1:
-                    self.gui.hotkeys=hotkeys
-        except Exception:
-            pass
-        
-
-    def walk(self,src):
-        for root,dirs,files in os.walk(src,topdown=True):
-            dirs[:] = [d for d in dirs if d not in self.exclude]
-            for name in files:
-                ext = name.split(".")[len(name.split("."))-1].lower()
-                if ext == "png" or ext == "gif" or ext == "jpg" or ext == "jpeg" or ext == "bmp" or ext == "pcx" or ext == "tiff" or ext=="webp" or ext=="psd" or ext=="jfif":
-                    self.imagelist.append(Imagefile(name, os.path.join(root,name)))
-    def setDestination(self,dest):
-        pass
-    def validate(self,gui):
-        samepath =  (gui.sdpEntry.get() == gui.ddpEntry.get())
-        if((os.path.isdir(gui.sdpEntry.get())) and (os.path.isdir(gui.ddpEntry.get())) and not samepath):
-            self.sdp=gui.sdpEntry.get()
-            self.ddp=gui.ddpEntry.get()
-            self.setup(self.sdp,self.ddp)
-            gui.guisetup(self.destinations)
-        elif gui.sdpEntry.get() == gui.ddpEntry.get():
-            gui.sdpEntry.delete(0,len(gui.sdpEntry.get()))
-            gui.ddpEntry.delete(0,len(gui.ddpEntry.get()))
-            gui.sdpEntry.insert(0,"PATHS CANNOT BE SAME")
-            gui.ddpEntry.insert(0,"PATHS CANNOT BE SAME")
-        else:
-            gui.sdpEntry.delete(0,len(gui.sdpEntry.get()))
-            gui.ddpEntry.delete(0,len(gui.ddpEntry.get()))
-            gui.sdpEntry.insert(0,"ERROR INVALID PATH")
-            gui.ddpEntry.insert(0,"ERROR INVALID PATH")
-    def setup(self,src,dest):
-        #scan the destination
-        self.destinations=[]
-        with os.scandir(dest) as it:
-            for entry in it:
-                if entry.is_dir():
-                    self.destinations.append({'name': entry.name,'path': entry.path})
-        self.walk(src)
-    def getthumb(self,imagefile):
-        thumbsize=self.gui.thumbnailsize
-        __image = Image.open(imagefile.path)
-        MAX_SIZE=(thumbsize,thumbsize)
-        thumb=__image.thumbnail(MAX_SIZE)
-        thumb.save(os.path.join("data",))
-        imagefile.thumbnail=
-
-                
 
 
 class GUIManager(tk.Tk):
     thumbnailgrid=[3,3]
-    thumbnailsize=200
-    def __init__(self, fileManager=SortImages) -> None:
+    thumbnailsize=256
+    def __init__(self, fileManager) -> None:
         super().__init__()
         self.fileManager=fileManager
         self.geometry(str(self.winfo_screenwidth()-5)+"x"+str(self.winfo_screenheight()-120))
         self.geometry("+0+60")
         self.buttons = []
-        self.columns = 2
         self.hotkeys = "123456qwerty7890uiop[asdfghjkl;zxcvbnm,.!@#$%^QWERT&*()_UIOPASDFGHJKLZXCVBNM<>"
         #Paned window that holds the almost top level stuff.
         self.toppane= Panedwindow(self,orient=HORIZONTAL)
@@ -228,10 +144,17 @@ Thanks you for using this program!""")
         self.entryframe.grid(row=0,column=0,sticky="w")
         #Finish setup for the left hand bar.
         #Start the grid setup
-        self.imagegridframe = tk.Frame(self.toppane)
-        self.imagegridframe['background']='#a9a9a9' # set background color to a darker grey
-        self.imagegridframe.grid(row=0,column=1,sticky="NSEW")
-        self.toppane.add(self.imagegridframe,weight=3)
+        imagegridframe = tk.Frame(self.toppane)
+        imagegridframe.grid(row=0,column=1,sticky="NSEW")
+        vbar =AutoScrollbar(imagegridframe,orient='vertical')
+        vbar.grid(row=0, column=1, sticky='ns')
+        self.imagegrid=tk.Text(imagegridframe,wrap='char',borderwidth=0,highlightthickness=0,state="disabled")
+        self.imagegrid['background']='#a9a9a9' # set background color to a darker grey
+        self.imagegrid.grid(row=0,column=0,sticky="NSEW")
+        imagegridframe.rowconfigure(0, weight=1) 
+        imagegridframe.columnconfigure(0, weight=1)
+
+        self.toppane.add(imagegridframe,weight=3)
         self.toppane.grid(row=0,column=0,columnspan=200,rowspan=200,sticky="NSEW")
         self.toppane.configure()
         self.columnconfigure(0,weight=10)
@@ -242,10 +165,10 @@ Thanks you for using this program!""")
         self.winfo_toplevel().title("Simple Image Sorter v2.0")
         self.leftui.bind("<Configure>", self.buttonResizeOnWindowResize)
         self.buttonResizeOnWindowResize("a")
-        self.mainloop()
     def closeprogram(self):
         saveprefs(self.fileManager, self)
         self.destroy()
+        shutil.rmtree("data",True)
         exit(0)
     def excludeshow(self):
         excludewindow = tk.Toplevel()
@@ -265,13 +188,38 @@ Thanks you for using this program!""")
             toplevelwin.destroy()
         except:
             pass
+    def makegridsquare(self,parent,imageobj):
+        frame = tk.Frame(parent,relief=RAISED,width=self.thumbnailsize)
+        try: 
+            frame.uuid=imageobj.id
+            canvas = tk.Canvas(width=self.thumbnailsize,height=self.thumbnailsize)
+            img= PhotoImage(imageobj.thumbnail)
+            canvas.create_image(self.thumbnailsize,self.thumbnailsize,image=img,anchor="nw")
+            canvas.grid(column=0,row=0,sticky="NSEW")
+            check=Checkbutton(frame, text=imageobj.name,variable=imageobj.checked)
+            check.grid(column=0,row=1)
+            canvas.imagetk=img
+            canvas.bind("<Button-2>", partial(self.displayimage,imageobj.path))
+            frame.bind("<Button-1>",check.invoke()) #anything other than rightclicking toggles the checkbox, as we want.
+            canvas.bind("<Button-1>",check.invoke())
+        except Exception as e:
+            logging.error(e)
+
+        return frame
+    def displaygrid(self,imagelist,range):
+        for i in range:
+            logging.debug(i)
+            gridsqaure = self.makegridsquare(self.imagegrid,imagelist[i])
+            gridsqaure.grid(row=0,column=0, ipadx=4,ipady=4)
+
+
 
     def buttonResizeOnWindowResize(self, b=""):
         if len(self.buttons) > 0:
             for x in self.buttons:
                 x.configure(wraplength=(self.buttons[0].winfo_width()-1))
     def displayimage(self, path):
-        print("Displaying:" + path)
+        logging.INFO("Displaying:" + path)
         self.imagewindow = tk.Toplevel()
         self.imagewindow.rowconfigure(0, weight=1)
         self.imagewindow.columnconfigure(0, weight=1)
@@ -302,7 +250,6 @@ Thanks you for using this program!""")
         panel = self.panel
         buttonframe = self.buttonframe
         hotkeys = self.hotkeys
-        columns = self.columns
         for x in self.buttons:
             x.destroy()  # clear the gui
         panel.destroy()
@@ -310,7 +257,6 @@ Thanks you for using this program!""")
         guicol = 0
         itern = 0
         smallfont = tkfont.Font(family='Helvetica', size=10)
-        columns = 2
         if len(destinations) > int((self.leftui.winfo_height()/15)-4):
             columns = 3
         for x in destinations:
@@ -348,6 +294,101 @@ Thanks you for using this program!""")
         ddpEntry.config(state=tk.DISABLED)
         # zoom
 
+
+class SortImages:
+    imagelist = deque()
+    destinations=[]
+    exclude=[]
+    thumbnailsize=256
+    def __init__(self) -> None:
+        self.gui = GUIManager(self)
+        if(os.path.exists("data") and os.path.isdir("data")):
+            pass
+        else:
+            os.mkdir(os.path.sep+"data")
+        hotkeys = ""
+        try:
+            with open("prefs.json","r") as prefsfile:
+                jdata=prefsfile.read()
+                jprefs=json.loads(jdata)
+                if 'hotkeys' in jprefs:
+                    hotkeys = jprefs["hotkeys"]
+                if 'thumbnailgrid' in jprefs:
+                    self.gui.thumbnailgrid = jprefs["thumbnailgrid"]
+                if 'thumbnailsize' in jprefs:
+                    self.gui.thumbnailsize = jprefs["thumbnailsize"]
+                    self.thumbnailsize = jprefs["thumbnailsize"]
+                self.exclude = jprefs["exclude"]
+                self.gui.sdpEntry.delete(0,len(self.gui.sdpEntry.get()))
+                self.gui.ddpEntry.delete(0,len(self.gui.ddpEntry.get()))
+                self.gui.sdpEntry.insert(0,jprefs["srcpath"])
+                self.gui.ddpEntry.insert(0,jprefs["despath"])
+            if len(hotkeys) > 1:
+                    self.gui.hotkeys=hotkeys
+        except Exception as e:
+            logging.error(e)
+        self.gui.mainloop()
+        
+
+    def walk(self,src):
+        for root,dirs,files in os.walk(src,topdown=True):
+            dirs[:] = [d for d in dirs if d not in self.exclude]
+            for name in files:
+                ext = name.split(".")[len(name.split("."))-1].lower()
+                if ext == "png" or ext == "gif" or ext == "jpg" or ext == "jpeg" or ext == "bmp" or ext == "pcx" or ext == "tiff" or ext=="webp" or ext=="psd" or ext=="jfif":
+                    imgfile = Imagefile(name, os.path.join(root,name))
+                    self.imagelist.append(imgfile)
+        return self.imagelist
+    def setDestination(self,dest):
+        pass
+    def validate(self,gui):
+        samepath =  (gui.sdpEntry.get() == gui.ddpEntry.get())
+        if((os.path.isdir(gui.sdpEntry.get())) and (os.path.isdir(gui.ddpEntry.get())) and not samepath):
+            self.sdp=gui.sdpEntry.get()
+            self.ddp=gui.ddpEntry.get()
+            self.setup(self.sdp,self.ddp)
+            gui.guisetup(self.destinations)
+            gui.displaygrid(self.imagelist,range(0,10))
+        elif gui.sdpEntry.get() == gui.ddpEntry.get():
+            gui.sdpEntry.delete(0,len(gui.sdpEntry.get()))
+            gui.ddpEntry.delete(0,len(gui.ddpEntry.get()))
+            gui.sdpEntry.insert(0,"PATHS CANNOT BE SAME")
+            gui.ddpEntry.insert(0,"PATHS CANNOT BE SAME")
+        else:
+            gui.sdpEntry.delete(0,len(gui.sdpEntry.get()))
+            gui.ddpEntry.delete(0,len(gui.ddpEntry.get()))
+            gui.sdpEntry.insert(0,"ERROR INVALID PATH")
+            gui.ddpEntry.insert(0,"ERROR INVALID PATH")
+    def setup(self,src,dest):
+        #scan the destination
+        self.destinations=[]
+        with os.scandir(dest) as it:
+            for entry in it:
+                if entry.is_dir():
+                    self.destinations.append({'name': entry.name,'path': entry.path})
+        self.generatethumbnails(self.walk(src))
+
+    def makethumb(self,imagefile):
+        try:
+            thumbsize=self.thumbnailsize
+            im = Image.open(imagefile.path)
+            if im.mode in ("RGBA", "P"): im = im.convert("RGB") #discard transparency
+            MAX_SIZE=(thumbsize,thumbsize)
+            im.thumbnail(MAX_SIZE)
+            thumbpath=os.path.join("data",imagefile.id+os.extsep+"jpg")
+            im.save(thumbpath)
+            imagefile.thumbnail=thumbpath
+        except Exception as e:
+            logging.error("Error:: %s",e)
+    def generatethumbnails(self,images):
+        with concurrent.ThreadPoolExecutor(max_workers=3) as executor:
+            executor.map(self.makethumb, images)
+        logging.debug("Finished making thumbnails")
+
+
+
 # Run Program
 if __name__ == '__main__':
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.DEBUG,datefmt="%H:%M:%S")
     mainclass = SortImages()
