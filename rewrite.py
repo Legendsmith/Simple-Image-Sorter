8ec01a4459aba98f85f26c24bc7dd7d06c03abfe
Legendsmith <legendsmith.au@gmail.com>
2022-01-26 19:21:04 +1100

    Added a lot of functionality, but it's broken
    We display in grid squares now and assign batches of image files at once!
    Old large view can be achieved by rightclicking on image thumbnails. Generates thumbnails
    Images displaying
    added initial code to move things
    lots of GUI updates for QoL and functionality.
    - can show and hide image

--- rewrite.py
+++ rewrite.py
@@ -1,7 +1,12 @@
+from distutils.log import debug
+import math
 import os
+from pickle import FALSE
 import shutil
 from sys import exit
-from tkinter import HORIZONTAL, RAISED, PhotoImage, messagebox
+from textwrap import wrap
+import textwrap
+from tkinter import HORIZONTAL, messagebox
 from shutil import move as shmove
 import tkinter as tk
 import tkinter.scrolledtext as tkst
@@ -17,27 +22,38 @@ import tkinter.font as tkfont
 from collections import deque
 from canvasimage import CanvasImage
 import concurrent.futures as concurrent
-import uuid
 import logging
-from autoscrollbar import AutoScrollbar
+from hashlib import md5
+
 
 class Imagefile:
     name=""
     path=""
-    dest=""
     moved=False
     def __init__(self,name,path) -> None:
         self.name=name
         self.path=path
-        self.id=str(uuid.uuid1())
-        self.checked=tk.StringVar()
+        self.checked=tk.IntVar(value=0)
     def move(self):
-        if self.dest != "":
+        destpath = self.dest["path"]
+        if destpath != "" and os.path.isdir(destpath):
             try:
-                shmove(self.path, os.path.join(self.dest,self.name))
+                shmove(self.path, os.path.join(destpath,self.name))
                 self.moved=True
+                self.guidata["frame"].configure(highlightbackground="green", highlightthickness=2)
+                self.path=os.path.join(destpath,self.name)
+                destpath=""
+                return "Moved:" + self.name + " -> " + destpath
             except Exception as e:
-                logging.error("Error: %s . File: %s",e,self.name)
+                logging.error("Error moving: %s . File: %s",e,self.name)
+                return ("Error moving: %s . File: %s",e,self.name)
+    def setid(self,id):
+        self.id =id
+    def setguidata(self,data):
+        self.guidata=data
+    def setdest(self,dest):
+        logging.debug("Set destination of %s to %s",self.name,dest["path"])
+        self.dest = dest
 
     
 
@@ -84,13 +100,22 @@ def saveprefs(manager,gui):
     except Exception:
         pass
 
-
+def bindhandler(*args):
+    widget = args[0]
+    command=args[1]
+    if command=="invoke":
+        widget.invoke()
+    elif command=="destroy":
+        widget.destroy()
+    elif command=="scroll":
+        widget.yview_scroll(-1*math.floor(args[2].delta/120), "units")
 
 class GUIManager(tk.Tk):
     thumbnailgrid=[3,3]
     thumbnailsize=256
     def __init__(self, fileManager) -> None:
         super().__init__()
+        self.gridsquarelist=[]
         self.fileManager=fileManager
         self.geometry(str(self.winfo_screenwidth()-5)+"x"+str(self.winfo_screenheight()-120))
         self.geometry("+0+60")
@@ -111,7 +136,7 @@ Enter a root folder to sort to for the "Destination field" too. The destination 
 It is reccomended that the folder names are not super long. You can always rename them later if you desire longer names. Exclusions let you ignore folder names. They are saved (unless you delete prefs.json). Remember that it's one per line, no commas or anything.
 You can change the hotkeys in prefs.json, just type a string of letters and numbers and it'll use that. It differentiates between lower and upper case (anything that uses shift), but not numpad.
 Thanks to FooBar167 on stackoverflow for the advanced (and memory efficient!) Zoom and Pan tkinter class.
-You can use arrow keys or click and drag to pan the image. Mouse Wheel Zooms the image.
+Rightclick a thumbnail to show the full image. You can use arrow keys or click and drag to pan the popout image. Mouse Wheel Zooms the image.
 Thanks you for using this program!""")
         self.panel.grid(row=11, column=0, columnspan=200,
                         rowspan=200, sticky="NSEW")
@@ -146,9 +171,10 @@ Thanks you for using this program!""")
         #Start the grid setup
         imagegridframe = tk.Frame(self.toppane)
         imagegridframe.grid(row=0,column=1,sticky="NSEW")
-        vbar =AutoScrollbar(imagegridframe,orient='vertical')
+        self.imagegrid=tk.Text(imagegridframe,wrap='word',borderwidth=0,highlightthickness=0,state="disabled")
+        vbar =tk.Scrollbar(imagegridframe,orient='vertical',command=self.imagegrid.yview)
         vbar.grid(row=0, column=1, sticky='ns')
-        self.imagegrid=tk.Text(imagegridframe,wrap='char',borderwidth=0,highlightthickness=0,state="disabled")
+        self.imagegrid.configure(yscrollcommand=vbar.set)
         self.imagegrid['background']='#a9a9a9' # set background color to a darker grey
         self.imagegrid.grid(row=0,column=0,sticky="NSEW")
         imagegridframe.rowconfigure(0, weight=1) 
@@ -165,10 +191,34 @@ Thanks you for using this program!""")
         self.winfo_toplevel().title("Simple Image Sorter v2.0")
         self.leftui.bind("<Configure>", self.buttonResizeOnWindowResize)
         self.buttonResizeOnWindowResize("a")
+        # options frame
+        optionsframe = tk.Frame(self.leftui)
+        self.hideonassignvar=tk.BooleanVar() #have this just get checked when setting dstination then hide or not
+        hideonassign = tk.Checkbutton(optionsframe,text="Hide Images Upon assigning destination",variable=self.hideonassignvar, onvalue=True,offvalue=False)
+        hideonassign.grid(column=0,row=0)
+        self.hidemovedvar=tk.BooleanVar()
+        self.showhiddenvar=tk.BooleanVar()
+        showhidden= tk.Checkbutton(optionsframe,text="Show Hidden Images",variable=self.showhiddenvar, onvalue=True,offvalue=False)
+        showhidden.grid(column=0,row=1)
+        hidemoved=tk.Checkbutton(optionsframe,text="Hide Moved",variable=self.hidemovedvar,onvalue=True,offvalue=False)
+        hidemoved.grid(column=1,row=1)
+        self.showhidden = showhidden
+        self.hideonassign=hideonassign
+        hideonassign.grid(column=1,row=0)
+        moveallbutton=tk.Button(optionsframe,text="Move All",command=fileManager.moveall)
+        moveallbutton.grid(column=0,row=2,sticky="EW")
+
+        self.optionsframe=optionsframe
+    def showall(self):
+        for x in self.fileManager.imagelist:
+            if x.guidata["show"]==False:
+                x.guidata["frame"].grid()
+        self.hidemoved()
+        self.hideassignedsquare(self.fileManager.imagelist)
     def closeprogram(self):
         saveprefs(self.fileManager, self)
         self.destroy()
-        shutil.rmtree("data",True)
+        #shutil.rmtree("data",True)
         exit(0)
     def excludeshow(self):
         excludewindow = tk.Toplevel()
@@ -189,28 +239,37 @@ Thanks you for using this program!""")
         except:
             pass
     def makegridsquare(self,parent,imageobj):
-        frame = tk.Frame(parent,relief=RAISED,width=self.thumbnailsize)
         try: 
-            frame.uuid=imageobj.id
-            canvas = tk.Canvas(width=self.thumbnailsize,height=self.thumbnailsize)
-            img= PhotoImage(imageobj.thumbnail)
-            canvas.create_image(self.thumbnailsize,self.thumbnailsize,image=img,anchor="nw")
+            img= ImageTk.PhotoImage(Image.open(imageobj.thumbnail))
+            frame=tk.Frame(parent,width=self.thumbnailsize+14,height=self.thumbnailsize+24)
+            canvas = tk.Canvas(frame,width=self.thumbnailsize,height=self.thumbnailsize)
+            canvas.uuid=imageobj.id
+            canvas.img=img
+            canvas.create_image(self.thumbnailsize/2,self.thumbnailsize/2,anchor="center",image=img)
+            
+            text = textwrap.fill(imageobj.name,math.floor(self.thumbnailsize/12))
+            check=Checkbutton(frame, text=text,variable=imageobj.checked)
             canvas.grid(column=0,row=0,sticky="NSEW")
-            check=Checkbutton(frame, text=imageobj.name,variable=imageobj.checked)
-            check.grid(column=0,row=1)
-            canvas.imagetk=img
-            canvas.bind("<Button-2>", partial(self.displayimage,imageobj.path))
-            frame.bind("<Button-1>",check.invoke()) #anything other than rightclicking toggles the checkbox, as we want.
-            canvas.bind("<Button-1>",check.invoke())
+            check.grid(column=0,row=1,sticky="N")
+            frame.rowconfigure(0,weight=4)
+            frame.rowconfigure(1,weight=1)
+            frame.config(height=self.thumbnailsize+12)
+            # save the data to the image obj to both store a reference and for later manipulation
+            imageobj.setguidata({"img":img,"frame":frame,"check":check,"show":True})
+            canvas.bind("<Button-1>",partial(bindhandler,check,"invoke")) #anything other than rightclicking toggles the checkbox, as we want.
+            canvas.bind("<Button-3>", partial(self.displayimage,imageobj.path))
+            check.bind("<Button-3>", partial(self.displayimage,imageobj.path))
+            self.gridsquarelist.append(frame)
         except Exception as e:
             logging.error(e)
 
         return frame
     def displaygrid(self,imagelist,range):
         for i in range:
-            logging.debug(i)
-            gridsqaure = self.makegridsquare(self.imagegrid,imagelist[i])
-            gridsqaure.grid(row=0,column=0, ipadx=4,ipady=4)
+            gridsquare = self.makegridsquare(self.imagegrid,imagelist[i])
+            self.imagegrid.window_create("insert",window=gridsquare)
+            gridsquare.configure(height=self.thumbnailsize+10)
+            gridsquare.bind_all("<MouseWheel>",partial(bindhandler,self.imagegrid,"scroll"))
 
 
 
@@ -218,21 +277,23 @@ Thanks you for using this program!""")
         if len(self.buttons) > 0:
             for x in self.buttons:
                 x.configure(wraplength=(self.buttons[0].winfo_width()-1))
-    def displayimage(self, path):
-        logging.INFO("Displaying:" + path)
-        self.imagewindow = tk.Toplevel()
-        self.imagewindow.rowconfigure(0, weight=1)
-        self.imagewindow.columnconfigure(0, weight=1)
-        self.imagewindow.wm_title("Image: "+ path)
-        self.imagewindow.geometry(str(
+    def displayimage(self, path,a):
+        logging.info("Displaying:" + path)
+        imagewindow = tk.Toplevel()
+        imagewindow.rowconfigure(0, weight=1)
+        imagewindow.columnconfigure(0, weight=1)
+        imagewindow.wm_title("Image: "+ path)
+        imagewindow.geometry(str(
             int(self.winfo_screenwidth()*0.80)) + "x" + str(self.winfo_screenheight()-120))
-        self.imagewindow.geometry("+365+60")
+        imagewindow.geometry("+365+60")
         self.winfo_toplevel().title("SIS: " + path)
-        imageframe = CanvasImage(self.imagewindow, path)
+        imageframe = CanvasImage(imagewindow, path)
         # takes the smaller scale (since that will be the limiting factor) and rescales the image to that so it fits the frame.
-        imageframe.rescale(min(self.imagewindow.winfo_width(
-        )/imageframe.imwidth, self.imagewindow.winfo_height()/imageframe.imheight))
+        imageframe.rescale(min(imagewindow.winfo_width(
+        )/imageframe.imwidth, imagewindow.winfo_height()/imageframe.imheight))
         imageframe.grid(column=0, row=0,)
+        imagewindow.bind("<Button-3>",partial(bindhandler,imagewindow,"destroy"))
+        self.imagewindow = imagewindow
 
     def folderselect(self, _type):
         folder = tkFileDialog.askdirectory()
@@ -260,15 +321,14 @@ Thanks you for using this program!""")
         if len(destinations) > int((self.leftui.winfo_height()/15)-4):
             columns = 3
         for x in destinations:
+            color = x['color']
             if x['name'] != "SKIP" and x['name'] != "BACK":
                 if(itern < len(hotkeys)):
                     newbut = tk.Button(buttonframe, text=hotkeys[itern] + ": " + x['name'], command=partial(
-                        self.fileManager.setDestination, x['path']), anchor="w", wraplength=(self.leftui.winfo_width()/columns)-1)
+                        self.fileManager.setDestination, x), anchor="w", wraplength=(self.leftui.winfo_width()/columns)-1)
                     random.seed(x['name'])
-                    self.bind_all(hotkeys[itern], partial(self.fileManager.setDestination, x['path']))
-                    color = randomColor()
+                    self.bind_all(hotkeys[itern], partial(self.fileManager.setDestination, x))                
                     fg = 'white'
-
                     if luminance(color) == 'light':
                         fg = "black"
                     newbut.configure(bg=color, fg=fg)
@@ -276,7 +336,7 @@ Thanks you for using this program!""")
                         newbut.configure(font=smallfont)
                 else:
                     newbut = tk.Button(buttonframe, text=x['name'], command=partial(
-                        self.fileManager.setDestination, x['path']), anchor="w")
+                        self.fileManager.setDestination, x), anchor="w")
                 itern += 1
             newbut.config(font=("Courier", 12), width=int(
                 (self.leftui.winfo_width()/12)/columns), height=1)
@@ -285,14 +345,45 @@ Thanks you for using this program!""")
             if guirow > ((self.leftui.winfo_height()/35)-2):
                 guirow = 1
                 guicol += 1
+            x["color"]=color
             newbut.grid(row=guirow, column=guicol, sticky="nsew")
+            newbut.bind("<Button-3>",partial(self.showthisdest,x))
+            newbut.dest=x
             self.buttons.append(newbut)
             guirow += 1
-
-        #textout.grid(column=0,row=0, sticky="nsew")
         sdpEntry.config(state=tk.DISABLED)
         ddpEntry.config(state=tk.DISABLED)
-        # zoom
+        self.entryframe.grid_remove()
+        self.optionsframe.grid(row=0,column=0,sticky="w")
+    def hideassignedsquare(self,imlist):
+        if self.hideonassignvar.get():
+            for x in imlist:
+                x.guidata["frame"].grid_remove()
+                x.guidata["show"]=False
+    def hideallsquares(self):
+        for x in self.gridsquarelist:
+            x.grid_remove()
+    def showunassigned(self,imlist):
+        for x in imlist:
+            if x.guidata["show"] or x.dest=="":
+                 x.guidata["frame"].grid()
+        self.hideassignedsquare(imlist)
+    def showthisdest(self,dest):
+        self.hideallsquares()
+        for x in self.fileManager.imagelist:
+            if x.dest == dest:
+                x.guidata["frame"].grid()
+    def hidemoved(self):
+        if self.hidemovedvar.get():
+            for x in self.fileManager.imagelist:
+                if x.moved:
+                    x.guidata["frame"].grid_remove()
+
+
+
+
+
+
 
 
 class SortImages:
@@ -302,6 +393,7 @@ class SortImages:
     thumbnailsize=256
     def __init__(self) -> None:
         self.gui = GUIManager(self)
+        #note, just load the preferences then pass it to the guimanager for processing there
         if(os.path.exists("data") and os.path.isdir("data")):
             pass
         else:
@@ -329,26 +421,54 @@ class SortImages:
             logging.error(e)
         self.gui.mainloop()
         
+    def moveall(self):
+        loglist=[]
+        for x in self.imagelist:
+            loglist.append(x.move())
+        try:
+            with open("filelog.txt", "a") as logfile:
+                logfile.writelines(loglist)
+        except:
+            logging.error("Failed to write filelog.txt")
+            
+
 
     def walk(self,src):
         for root,dirs,files in os.walk(src,topdown=True):
             dirs[:] = [d for d in dirs if d not in self.exclude]
             for name in files:
                 ext = name.split(".")[len(name.split("."))-1].lower()
-                if ext == "png" or ext == "gif" or ext == "jpg" or ext == "jpeg" or ext == "bmp" or ext == "pcx" or ext == "tiff" or ext=="webp" or ext=="psd" or ext=="jfif":
+                if ext == "png" or ext == "gif" or ext == "jpg" or ext == "jpeg" or ext == "bmp" or ext == "pcx" or ext == "tiff" or ext=="webp" or ext=="psd":
                     imgfile = Imagefile(name, os.path.join(root,name))
                     self.imagelist.append(imgfile)
         return self.imagelist
     def setDestination(self,dest):
-        pass
+        marked = []
+        for x in self.imagelist:
+            if x.checked.get()==1:
+                marked.append(x)
+        for obj in marked:
+            obj.setdest(dest)
+            obj.guidata["frame"]['background']=dest['color']
+            obj.guidata["check"].invoke()
+        self.gui.hideassignedsquare(marked)
+        
+        
+
+            
+            
+        
     def validate(self,gui):
         samepath =  (gui.sdpEntry.get() == gui.ddpEntry.get())
         if((os.path.isdir(gui.sdpEntry.get())) and (os.path.isdir(gui.ddpEntry.get())) and not samepath):
             self.sdp=gui.sdpEntry.get()
             self.ddp=gui.ddpEntry.get()
+            logging.info("main class setup")
             self.setup(self.sdp,self.ddp)
+            logging.info("GUI setup")
             gui.guisetup(self.destinations)
-            gui.displaygrid(self.imagelist,range(0,10))
+            logging.info("displaying first image grid")
+            gui.displaygrid(self.imagelist,range(0,len(self.imagelist)))
         elif gui.sdpEntry.get() == gui.ddpEntry.get():
             gui.sdpEntry.delete(0,len(gui.sdpEntry.get()))
             gui.ddpEntry.delete(0,len(gui.ddpEntry.get()))
@@ -365,22 +485,30 @@ class SortImages:
         with os.scandir(dest) as it:
             for entry in it:
                 if entry.is_dir():
-                    self.destinations.append({'name': entry.name,'path': entry.path})
+                    self.destinations.append({'name': entry.name,'path': entry.path, 'color': randomColor()})
         self.generatethumbnails(self.walk(src))
 
     def makethumb(self,imagefile):
-        try:
-            thumbsize=self.thumbnailsize
-            im = Image.open(imagefile.path)
-            if im.mode in ("RGBA", "P"): im = im.convert("RGB") #discard transparency
-            MAX_SIZE=(thumbsize,thumbsize)
-            im.thumbnail(MAX_SIZE)
-            thumbpath=os.path.join("data",imagefile.id+os.extsep+"jpg")
-            im.save(thumbpath)
+        with Image.open(imagefile.path) as img:
+            hash=md5()
+            hash.update(img.tobytes())
+            imagefile.setid(hash.hexdigest())
+        thumbpath=os.path.join("data",imagefile.id+os.extsep+"jpg")
+        if  os.path.exists(thumbpath):
             imagefile.thumbnail=thumbpath
-        except Exception as e:
-            logging.error("Error:: %s",e)
+        else:
+            try:
+                thumbsize=self.thumbnailsize
+                im = Image.open(imagefile.path)
+                if im.mode in ("RGBA", "P"): im = im.convert("RGB") #discard transparency
+                MAX_SIZE=(thumbsize,thumbsize)
+                im.thumbnail(MAX_SIZE)
+                im.save(thumbpath)
+                imagefile.thumbnail=thumbpath
+            except Exception as e:
+                logging.error("Error:: %s",e)
     def generatethumbnails(self,images):
+        logging.info(",md5 hashing %s files",len(images))
         with concurrent.ThreadPoolExecutor(max_workers=3) as executor:
             executor.map(self.makethumb, images)
         logging.debug("Finished making thumbnails")
