6ff26acbbe3b4c37aab4896a3b22e56e4e325b42
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2024-09-22 20:27:20 +0300

    Add checkbox to check sorting preference

--- gui.py
+++ gui.py
@@ -79,6 +79,7 @@ class GUIManager(tk.Tk):
         self.hideonassignvar.set(True)
         self.hidemovedvar = tk.BooleanVar()
         self.showhiddenvar = tk.BooleanVar()
+        self.sortbydate = tk.BooleanVar()
         self.squaresperpage = tk.IntVar()
         self.squaresperpage.set(120)
         self.sessionpathvar = tk.StringVar()
@@ -98,6 +99,9 @@ class GUIManager(tk.Tk):
         #self.leftui.grid(row=0, column=0, sticky="NESW")
         self.leftui.columnconfigure(0, weight=1)
         self.toppane.add(self.leftui, weight=1)
+        #Add a checkbox to check for sorting preference.
+        self.sortbydatecheck = tk.Checkbutton(self.leftui, text="Sort by Date", variable=self.sortbydate, onvalue=True, offvalue=False, command=self.sortbydate)
+        self.sortbydatecheck.grid(row=2, column=0, sticky="w", padx=25)
         self.panel = tk.Label(self.leftui, wraplength=300, justify="left", text="""Select a source directory to search for images in above.
 The program will find all png, gif, jpg, bmp, pcx, tiff, Webp, and psds. It can has as many sub-folders as you like, the program will scan them all (except exclusions).
 Enter a root folder to sort to for the "Destination field" too. The destination directory MUST have sub folders, since those are the folders that you will be sorting to.
