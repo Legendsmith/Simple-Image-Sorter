ddfb7ac3d581a7d02c6d11b17c045d5cb1dda812
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2025-02-04 13:33:04 +0200

    Use newest pyvips
    Copy pyvips windows binaries to _internal folder once pyinstaller is done.

--- build.bat
+++ build.bat
@@ -1,7 +1,35 @@
-cd %~dp0
+@echo off
+setlocal
+
+:: Set the name of the folder to delete
+set "folderToDelete=dist"
+set "folderToDelete2=build"
+if exist "%folderToDelete%" (
+    rmdir /s /q "%folderToDelete%"
+    echo Deleted folder: %folderToDelete%
+) else (
+    echo Folder not found: %folderToDelete%
+)
+if exist "%folderToDelete2%" (
+    rmdir /s /q "%folderToDelete2%"
+    echo Deleted folder: %folderToDelete2%
+) else (
+    echo Folder not found: %folderToDelete2%
+)
+
 pyinstaller sortimages_multiview.py --noconfirm
-copy .\libglib-2.0-0.dll .\dist\sortimages_multiview\libglib-2.0-0.dll
-copy .\libgobject-2.0-0.dll .\dist\sortimages_multiview\libgobject-2.0-0.dll
-copy .\libvips-42.dll .\dist\sortimages_multiview\libvips-42.dll
-copy .\libvips-cpp-42.dll .\dist\sortimages_multiview\libvips-cpp-42.dll
+
+:: Set the name of the file to copy and the destination for the file
+set "folderToCopy=vips-dev-8.16"  :: Change this to your actual file name
+set "folderCopyDestination=dist\SIME-QOL\_internal\vips-dev-8.16"  :: Change this to your actual destination folder
+
+:::: Copy the folder to the destination folder
+if exist "%folderToCopy%" (
+    xcopy "%folderToCopy%\*" "%folderCopyDestination%" /s /e /i /y
+    echo Copied folder: %folderToCopy% to %folderCopyDestination%
+) else (
+    echo Folder not found: %folderToCopy%
+)
+
+endlocal
 pause
